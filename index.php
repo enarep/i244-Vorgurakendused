b46dbb61f32f1501e948165959708c5526de09d3
enarep <enarep@itcollege.ee>
2017-02-11 15:39:54 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Koduleht</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<h1>
+test
+</h1>
+<div>
+<img src="pilt_2551.jpg" alt="Kuu">
+</div>
+<div>
+<?php
+	echo 'Praegune PHP versioon' . phpversion();
+?>
+</div>
+</body>
+
+</html>
+
