0cad27ac57f3dd4aa378fa1c12348f3a183fc274
enarep <enarep@itcollege.ee>
2017-02-19 17:04:44 +0200

    lisatud koduülesanne

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Koduleht</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>Koduleht</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
 
 <body>
-<h1>
-test
-</h1>
-<div>
-<img src="silm.gif" alt="pilt"/>
-</div>
-<div>
-<?php
-	echo 'Praegune PHP versioon' . phpversion();
-?>
-</div>
-<div>
-<p>
- <a href="http://validator.w3.org/check?uri=referer">
-  <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
- </a>
-</p>
+	<h1>
+		test
+	</h1>
+	
+	<div>
+		<img src="silm.gif" alt="pilt"/>
+	</div>
+	
+	<div>
+		<?php
+			echo 'Praegune PHP versioon' . phpversion();
+		?>
+	</div>
+	
+	<div>
+		<p>
+			<a href="http://validator.w3.org/check?uri=referer">
+				<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+			</a>
+		</p>
 
-</div>
+	</div>
+	
+	
 </body>
 
 </html>
