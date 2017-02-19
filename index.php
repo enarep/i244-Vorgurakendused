04cb98ee28f2c88de4cf8c971fb7eaa17ec35e36
enarep <enarep@itcollege.ee>
2017-02-19 16:17:13 +0200

    uus pilt, html validator

--- index.php
+++ index.php
@@ -11,12 +11,20 @@
 test
 </h1>
 <div>
-<img src="pilt_2551.jpg" alt="Kuu">
+<img src="silm.gif" alt="pilt"/>
 </div>
 <div>
 <?php
 	echo 'Praegune PHP versioon' . phpversion();
 ?>
+</div>
+<div>
+<p>
+ <a href="http://validator.w3.org/check?uri=referer">
+  <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+ </a>
+</p>
+
 </div>
 </body>
 
