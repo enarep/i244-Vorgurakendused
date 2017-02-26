cd56f569ad4c4125d7c85f934ffd4ab0c8161a2e
enarep <enarep@itcollege.ee>
2017-02-26 21:38:24 +0200

    teise nädala asjad

--- index.php
+++ index.php
@@ -4,24 +4,70 @@
 		<meta charset="UTF-8">
 		<title>Koduleht</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<script>
+			
+				setTimeout("document.body.style.backgroundColor='#ffffff'", 100)
+				setTimeout("document.body.style.backgroundColor='#f2e6ff'", 200)
+				setTimeout("document.body.style.backgroundColor='#e6ccff'", 300)
+				setTimeout("document.body.style.backgroundColor='#d9b3ff'", 400)
+				setTimeout("document.body.style.backgroundColor='#cc99ff'", 500)
+				setTimeout("document.body.style.backgroundColor='#bf80ff'", 600)
+				setTimeout("document.body.style.backgroundColor='#b366ff'", 700)
+				setTimeout("document.body.style.backgroundColor='#a64dff'", 800)
+				setTimeout("document.body.style.backgroundColor='#9933ff'", 900)
+				setTimeout("document.body.style.backgroundColor='#a64dff'", 1000)
+				setTimeout("document.body.style.backgroundColor='#b366ff'", 1100)
+				setTimeout("document.body.style.backgroundColor='#bf80ff'", 1200)
+				setTimeout("document.body.style.backgroundColor='#cc99ff'", 1300)
+				setTimeout("document.body.style.backgroundColor='#d9b3ff'", 1400)
+				setTimeout("document.body.style.backgroundColor='#e6ccff'", 1500)
+				setTimeout("document.body.style.backgroundColor='#f2e6ff'", 1600)
+			
+		</script>
+		
 	</head>
 
 <body>
+    
+    <div align="right">
+		<table>
+            <tr>
+                <th><a href="login.html">Logi sisse</a></th>
+                <th><a href="register.html">Registreeru</a></th>
+            </tr>
+        </table>
+	</div>
+    
+        
 	<h1>
 		test
 	</h1>
-	
-	<div>
-		<img src="silm.gif" alt="pilt"/>
+    
+    <div align="center">
+		<table>
+            <tr width="640">
+                <th><a href="galerii.php">Galerii</a></th>
+                <th>Tühi</th>
+                <th>Tühi</th>
+                <th>Tühi</th>
+            </tr>
+        </table>
 	</div>
 	
-	<div>
+	
+	
+    
+	<div align="center">
+		<img src="silm.gif" alt="pilt" width="640" height="427"/>
+	</div>
+	
+	<div align="center">
 		<?php
 			echo 'Praegune PHP versioon' . phpversion();
 		?>
 	</div>
 	
-	<div>
+	<div align="center">
 		<p>
 			<a href="http://validator.w3.org/check?uri=referer">
 				<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
