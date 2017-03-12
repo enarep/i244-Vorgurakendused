94a414ce4695d2921d280791809ca08f0492f965
enarep <enarep@itcollege.ee>
2017-03-12 22:06:53 +0200

    uus layout

--- index.php
+++ index.php
@@ -4,64 +4,47 @@
 		<meta charset="UTF-8">
 		<title>Koduleht</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="js/backgr.js"></script>
-
-			 
-		
-		
+			
 	</head>
 
 <body>
-    
-    <div align="right">
-		<table>
-            <tr>
-                <th><a href="login.html">Logi sisse</a></th>
-                <th><a href="register.html">Registreeru</a></th>
-            </tr>
-        </table>
-	</div>
-    
+    <div class='circleBase outer'>
+        <div class='circleBase inner'>
+            <table align='center'>
+                <tr>
+                    <th><a href="login.html"><h3 class="curve">Logi sisse</h3></a><a href="register.html"><h3 class="curve">Registreeru</h3></a><a href="galerii.php"><h3 class="curve">Galerii</h3></a></th>
+                </tr>
+            </table>
+        </div>
+     </div>
         
-	<h1>
-		test
-	</h1>
-    
-    <div align="center">
-		<table>
-            <tr width="640">
-                <th><a href="galerii.php">Galerii</a></th>
-                <th>Tühi</th>
-                <th>Tühi</th>
-                <th>Tühi</th>
-            </tr>
-        </table>
-	</div>
-	
-	
-	
-    
-	<div align="center">
-		<img class="gif" src="img/silm.gif" alt="pilt" width="640" height="427"/>
-	</div>
-	
-	<div align="center">
-		<?php
-			echo 'Praegune PHP versioon' . phpversion();
-		?>
-	</div>
-	
-	<div align="center">
-		<p>
-			<a href="http://validator.w3.org/check?uri=referer">
-				<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
-			</a>
-		</p>
+        <div align="center">
+            <h4 class="curve2">
+                <?php
+                    echo 'Praegune PHP versioon' . phpversion();
+                ?>
+            </h3>
+        </div>
 
-	</div>
+        <div align="center">
+            <p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+                </a>
+            </p>
+
+        </div>
+       
 	
 	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/jquery.arctext.js"></script>
+    <script>
+        var $curve = $('.curve');
+        var $curve2 = $('.curve2');
+        $curve.arctext({radius: 450});
+        $curve2.arctext({radius: 500, dir: -1});
+    </script>
 </body>
 
 </html>
