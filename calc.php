5964020154f8a2853086c9b80f7d0bdae3203fa0
enarep <enarep@itcollege.ee>
2017-05-28 12:11:06 +0300

    lisatud funktsionaalsust

--- calc.php
+++ calc.php
@@ -1,17 +1,53 @@
 <!DOCTYPE html>
 <?php
+
 include("funcs.php");
-if(isset($_POST['sub'])) {
+
+if(empty($radiovalue)){
+    $radiovalue = 'plainhex';
+}
+
+if (isset($_POST['sub'])) {
     
     $input = $_POST['sisend'];
     $radio = $_POST['convert'];
-    if($radio == 'hexplain'){
+    
+    if ($radio == 'hexplain'){
+        
         $result = hex2plain($input);
-    } elseif($radio == 'plainhex') {
+        
+    } elseif ($radio == 'plainhex') {
+        
         $result = plain2hex($input);
+        
+    } elseif ($radio == 'hexdec') {
+        
+        $result = hex2decimal($input);
+        
+    } elseif ($radio == 'dechex') {
+        
+        $result = dec2hexadec($input);
+        
+    } elseif ($radio == 'hexbin') {
+        
+        $result = hex2binary($input);
+        
+    } elseif ($radio == 'binhex') {
+        
+        $result = binary2hex($input);
+        
     }
     
+} elseif (isset($_POST['swap'])) {
+    
+    $swap = $_POST['valjund'];
+    
 }
+
+if(isset($_POST['convert'])){               //et raadionupp samas kohas püsiks peale refreshi
+    $radiovalue = $_POST['convert'];
+}
+ 
 
 ?>
 <html>
@@ -29,20 +65,52 @@ if(isset($_POST['sub'])) {
                 <table>
                     <tbody>
                         <tr>
-                            <label><input type="radio" name="convert" value="plainhex" id="plainhex" checked>Plaintext -> HEX</label>
-                            <label><input type="radio" name="convert" value="hexplain" id="hexplain">HEX -> plaintext</label>
+
+                            <label><input type="radio" name="convert" value="plainhex" id="plainhex" <?php if($radiovalue == "plainhex") { echo 'checked';} ?>>Plaintext -> HEX</label><br />
+
                         </tr>
                         <tr>
+
+                            <label><input type="radio" name="convert" value="hexplain" id="hexplain" <?php if($radiovalue == "hexplain") { echo 'checked';} ?>>HEX -> plaintext</label><br />
+
+                        </tr>
+                        <tr>
+
+                            <label><input type="radio" name="convert" value="hexdec" id="hexdec" <?php if($radiovalue == "hexdec") { echo 'checked';} ?>>HEX -> DEC</label><br />
+
+                        </tr>
+                        <tr>
+
+                            <label><input type="radio" name="convert" value="dechex" id="dechex" <?php if($radiovalue == "dechex") { echo 'checked';} ?>>DEC -> HEX</label><br />
+
+                        </tr>
+                        <tr>
+
+                            <label><input type="radio" name="convert" value="hexbin" id="hexbin" <?php if($radiovalue == "hexbin") { echo 'checked';} ?>>HEX -> BIN</label><br />
+
+                        </tr>
+                        <tr>
+
+                            <label><input type="radio" name="convert" value="binhex" id="binhex" <?php if($radiovalue == "binhex") { echo 'checked';} ?>>BIN -> HEX</label><br />
+                         </tr>
+
+                    
+                        <tr>
                             <td>Sisesta siia:</td>
-                            <td><input type="text" name="sisend"></td>
+                            <td><input type="text" name="sisend" value="<?php if (isset($swap)) echo $swap; ?>"></td>
                         </tr>
                         <tr>
                             <td>Tulemus:</td>
-                            <td><input type="text" name="valjund" value="<?php if (isset($result)) echo $result; ?>"></td>
+                                <td>
+                                    <textarea rows="3" name="valjund"><?php if (isset($result)) echo $result; ?></textarea>
+                                </td>
+                            <td><input type="submit" value="Sisendiks" name="swap"></td>
                         </tr>
                         
                         <tr>
+                            <td></td>
                             <td><input type="submit" value="Teisenda" name="sub"></td>
+                            
                         </tr>
                     </tbody>
                 </table>
--- funcs.php
+++ funcs.php
@@ -14,6 +14,63 @@ function plain2hex($plain){
     
 }
 
+function hex2decimal($hex){
+    
+    $dec = hexdec($hex);
+    return sprintf('%03d', $dec);
+    
+}
 
+function dec2binary($dec){
+    
+    $bin = decbin($dec);
+    return sprintf('%08d', $bin);
+    
+}
+
+function bin2decimal($bin){
+    
+    $dec = bindec($bin);
+    return $dec;
+    
+}
+
+function dec2hexadec($dec){
+    
+    $hex = dechex($dec);
+    return $hex;
+    
+}
+
+function hex2binary($hex){
+    
+    $max = strlen($hex);
+    $out = '';
+    
+    for ($i = 0; $i < $max; $i += 2){
+        
+        $byte = substr($hex, $i, 2);        //tõlgendab stringi tükkhaaval
+        $bin = decbin(hexdec($byte));
+        $out .= sprintf('%08d', $bin)." ";
+        
+    }
+    return $out;
+    
+}
+
+function binary2hex($bin){
+    
+    $new = '';
+    for ($char = 0; $char < strlen($bin); $char++){     //eemalda tühikud
+        
+        if ($bin[$char] != ' '){
+            $new .= $bin[$char];
+        } 
+    }
+    
+    $out = base_convert($new, 2, 16);
+    return $out;
+    
+}
 
 ?>
